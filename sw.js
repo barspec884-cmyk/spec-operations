4acce9652defbfd1af8de95aef545336c55af546
Mossan <bar.spec.884@gmail.com>
2026-01-02 14:39:06 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿const CACHE_NAME = 'spec-ops-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// キャッシュがあればキャッシュを返し、なければネットワークから取得
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
